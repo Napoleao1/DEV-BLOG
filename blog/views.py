0c43124d7ab9ef881a88e7c1de2d16c88711c5ef
Napoleao <ernaninandoxd@gmail.com>
2026-06-25 20:40:53 -0300

    criando as rotas e views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    
+    mensagem = "<h1> Bem vindo ao devblog </h1> <p> Em breve, artigos aqui. </p>"
+    
+    return HttpResponse(mensagem) 
+
+
+def sobre_nos(request):
+    
+    mensagem = "<h3>Sobre o Devblog </h3>"
+    
+    return HttpResponse(mensagem)
