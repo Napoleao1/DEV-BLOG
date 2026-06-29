786c2dfd371b6a6050ea2e31f40889fefdf852b8
Napoleao <ernaninandoxd@gmail.com>
2026-06-29 20:24:51 -0300

    adicionado index html e base html

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,11 @@ from django.http import HttpResponse
 
 def home(request):
     
-    mensagem = "<h1> Bem vindo ao devblog </h1> <p> Em breve, artigos aqui. </p>"
-    
+    return render(request, "blog/index.html")    
     return HttpResponse(mensagem) 
 
 
 def sobre_nos(request):
     
     mensagem = "<h3>Sobre o Devblog </h3>"
-    
     return HttpResponse(mensagem)
