543da5fb6d79f76415b139c7c06bf169cb8b8310
Napoleao <ernaninandoxd@gmail.com>
2026-06-30 21:10:05 -0300

    removido linhas não utilizadas

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,8 @@ from django.http import HttpResponse
 def home(request):
     
     return render(request, "blog/index.html")    
-    return HttpResponse(mensagem) 
 
 
 def sobre_nos(request):
     
     return render(request, "blog/sobre.html")
-    return HttpResponse(mensagem)
