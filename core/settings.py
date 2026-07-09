7cff8b3a95ff1906a3cb78c4856bd39ded6c34c3
Napoleao <ernaninandoxd@gmail.com>
2026-07-08 21:10:29 -0300

    feat: configura whitenoise para servir arquivos estaticos

--- core/settings.py
+++ core/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -130,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Arquivos de mídia (uploads dos usuários, ex: capas dos artigos)
